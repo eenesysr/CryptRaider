7617f561b51d83c93a3d8b8a14da84fd5dbcfd29
enesberkayyasar <2112101034@ogr.mehmetakif.edu.tr>
2024-09-30 14:58:14 +0300

    Real Time Add

--- Source/CryptRaider/Mover.cpp
+++ Source/CryptRaider/Mover.cpp
@@ -36,6 +36,7 @@ void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponent
 
 		FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
 		GetOwner()->SetActorLocation(NewLocation);
+			
 	}
 	
 	
